fe8725d697c5bfb507bb377ad47f754e11ef5e52
Wills-M <wmm38@drexel.edu>
2019-12-17 16:52:25 -0500

    Added rain.

---
+++ Assets/Scripts/RainFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform followTarget;
+
+    private Vector3 positionOffset;
+
+    void Start()
+    {
+        positionOffset = transform.position;
+    }
+    
+    void Update()
+    {
+        float x = followTarget.position.x + positionOffset.x * followTarget.right.x;
+        float y = positionOffset.y;
+        float z = followTarget.position.z + positionOffset.z * followTarget.forward.z;
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
